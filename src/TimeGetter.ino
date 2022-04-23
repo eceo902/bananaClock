5298653b0c9d222be2862ec0c5ac581d54bd7d04
Caroline Cunningham <ccunning@mit.edu>
2022-04-23 12:10:15 -0400

    store alarms as strs, delete alarms, use IMU to select them

---
+++ src/TimeGetter.ino
@@ -0,0 +1,78 @@
+class TimeGetter {
+    int hour_index;
+    int alphabet[62] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 
+    26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60};
+    char msg[400] = {0}; //contains previous query response
+    char query_string[50] = {0};
+    int state;
+    uint32_t scroll_timer;
+    const int scroll_threshold = 300;
+    const float angle_threshold = 0.3;
+
+  public:
+    TimeGetter() {
+      state = 0;
+      hour_index = 0;
+      scroll_timer = millis();
+    }
+  void scroll_prints(int hour_index, int max_val){
+      tft.fillScreen(TFT_BLACK);
+      tft.setCursor(0, 0, 1);
+      if (max_val == 23)
+      {
+        tft.println("Button39");
+        tft.println("Confirm Hour");
+      }
+      else if (max_val == 59){
+        tft.println("Button39");
+        tft.println("Confirm Minute");
+      }
+      tft.println("");
+      tft.printf("Selected: %d", hour_index);
+    }
+    int update(float angle, int button, int max_val) 
+    {
+      tft.setTextSize(1.75);
+      if (state == 0)
+      {
+        state = 1;
+      }
+      if (state == 1)
+      { //time selection
+        if((button==1) && (millis() - scroll_timer >= 100)) //PRESSED BUTTON TO SELECT GAME
+        { 
+          return hour_index;
+        } 
+        else if (scroll_threshold<=millis()-scroll_timer){
+        if (angle_threshold <= angle)
+        {
+          scroll_timer = millis();
+          scroll_prints(hour_index, max_val);
+          if(hour_index>=max_val){
+            hour_index=0;
+          }else{
+            hour_index+=1; 
+          }
+        } 
+        else if(angle<=-angle_threshold)
+        { 
+          scroll_timer = millis();
+          scroll_prints(hour_index, max_val);
+          if(hour_index<=0){
+              hour_index=max_val;
+          }else{
+              hour_index-=1;
+          }  
+        }
+  }
+  }
+  return -1;
+}
+};
+ 
+void get_angle(float* x, float* y) 
+{
+  imu.readAccelData(imu.accelCount);
+  *x = imu.accelCount[0] * imu.aRes;
+  *y = imu.accelCount[1] * imu.aRes;
+}
--- src/settings.ino
+++ src/settings.ino
@@ -1,11 +1,25 @@
-int alarm_hours[50];
-int alarm_minutes[50];
-int music_options[50];
+char alarm1Time[8];
+char alarm2Time[8];
+char alarm3Time[8];
+char alarm4Time[8];
+char alarm5Time[8];
+
+const int maxAlarmNums = 5;
+char *setting_alarms[maxAlarmNums] = {alarm1Time, alarm2Time, alarm3Time, alarm4Time, alarm5Time}; //https://www.javatpoint.com/cpp-array-of-pointers
+
+TimeGetter tg;
+
+// {id:time {time:, music}, id: {time:, music:}}
+
+// music_option -1 means alarm is deleted
+
+int music_options[maxAlarmNums];
+
+// format as 20 alarms
 
 const int BUTTON_PRESS_BUFFER = 250;
 
 int currNumberAlarms;
-
 int modAlarmNumber;
 
 const int PRINT_ALARMS = 0;
@@ -14,21 +28,31 @@ const int ADD = 2;
 const int MODIFY = 3;
 const int MODIFY_ALARM = 4;
 
+const int ADD_HOUR = 5;
+const int ADD_MIN = 6;
+const int ADD_MUSIC = 7;
+
+int alarm_state;
 
 int hour;
 int mins;
 int music_option;
-
-
 int setting_state;
+
+uint32_t scroll_timer = millis();
+const int scroll_threshold = 150;
+const float angle_threshold = 0.3;
+
 
 void setup_settings(){   // this is called when start setup
   currNumberAlarms = 0;
   setting_state = PRINT_ALARMS;
+  tft.setTextSize(1.5);
+  alarm_state = ADD_HOUR;
 }
 
-void handle_settings(){
-  // add an alarm if button 1 pushed
+void handle_settings()
+{
   if (setting_state == PRINT_ALARMS)
   {
     display_alarms();
@@ -36,16 +60,27 @@ void handle_settings(){
   }
   if (setting_state == DISP)
   {
+    // ADD SETTINGS - CHANGE BRIGHTNESS
+    // button34.read();
+    // if (button34.button_pressed && millis() - button34.button_change_time > BUTTON_PRESS_BUFFER)
+    // {
+    //   button34.button_change_time = millis();
+
+    // }
+
+    // ADD NEW ALARM
     button45.read();
-    if (button45.button_pressed && millis() - button45.button_change_time > BUTTON_PRESS_BUFFER)
+    if (button45.button_pressed && millis() - button45.button_change_time > BUTTON_PRESS_BUFFER && currNumberAlarms < maxAlarmNums)
     {
       button45.button_change_time = millis();
       hour = 1;
       mins = 0;
       music_option = 0;
       setting_state = ADD;
-      modify_prints(hour, mins, music_option);
+      scroll_timer = millis();
     }
+
+    // MODIFY ALARM
     button38.read();
     if (button38.button_pressed && millis() - button38.button_change_time > BUTTON_PRESS_BUFFER && currNumberAlarms > 0)
     {
@@ -54,10 +89,10 @@ void handle_settings(){
       setting_state = MODIFY;
       alarm_choice_prints();
     }
-
   }
-  if (setting_state == ADD){
-    modify_alarm(currNumberAlarms);
+  if (setting_state == ADD)
+  {
+    modify_alarm_2(currNumberAlarms);
   }
   if (setting_state == MODIFY){
     button39.read();
@@ -74,16 +109,9 @@ void handle_settings(){
     if (button34.button_pressed && millis() - button38.button_change_time > BUTTON_PRESS_BUFFER)
     {
       button34.button_change_time = millis();
-      if (alarm_hours[modAlarmNumber] == -1){
-        hour = 1;
-        mins = 0;
-        music_option = 0;
-      }
-      else{
-        hour = alarm_hours[modAlarmNumber];
-        mins = alarm_minutes[modAlarmNumber];
-        music_option = music_options[modAlarmNumber];
-      }
+      hour = 1;
+      mins = 0;
+      music_option = 0;
       setting_state = MODIFY_ALARM;
       modify_prints(hour, mins, music_option);
     }
@@ -94,15 +122,15 @@ void handle_settings(){
       delete_alarm(modAlarmNumber);
       setting_state = PRINT_ALARMS;
     }
-
   }
   if (setting_state == MODIFY_ALARM){
-    modify_alarm(modAlarmNumber);
+    modify_alarm_2(modAlarmNumber);
   }
 }
 
 void alarm_choice_prints(){
   display_alarms();
+  tft.println("");
   tft.println("Button39: Change ID");
   tft.println("Button34: Edit Alarm");
   tft.println("Button45: Delete Alarm");
@@ -113,25 +141,32 @@ void display_alarms()
 {
   tft.fillScreen(TFT_BLACK);
   tft.setCursor(0,0,1);
-  Serial.println(currNumberAlarms);
+  // Serial.println(currNumberAlarms);
   for (int i = 0; i < currNumberAlarms; i++)
   {
-    if (alarm_hours[i] != -1){
+    if (*setting_alarms[i] != '\0'){
       tft.printf("Alarm%d ", i+1);
-      print_time(alarm_hours[i], alarm_minutes[i]);
+      tft.printf("Time: %s", setting_alarms[i]);
       tft.printf(" Music:%d", music_options[i]);
       tft.println("");
     }
-    else{
-      tft.printf("Alarm Number %d:", i+1);
-      tft.println("OFF");
-    }
+    // else{
+    //   tft.printf("Alarm Number %d:", i+1);
+    //   tft.println("OFF");
+    // }
   }
   if (currNumberAlarms == 0){
     tft.println("No Alarms!");
   }
   if (setting_state != MODIFY){
-    tft.println("Button45: add new Alarm");
+    tft.println("");
+    if (currNumberAlarms < maxAlarmNums)
+    {
+      tft.println("Button45: add new Alarm");
+    }
+    else{
+      tft.println("Max Alarms Set!");
+    }
     if (currNumberAlarms != 0){
       tft.println("Button38: modify Alarm");
     }
@@ -139,25 +174,48 @@ void display_alarms()
 }
 
 void delete_alarm(int alarm_id){
-  alarm_hours[alarm_id] = -1;
-  alarm_minutes[alarm_id] = -1;
-  music_options[alarm_id] = -1;
+  // delete: move everything back one
+  Serial.println(alarm_id);
+  for (int i = alarm_id + 1; i < currNumberAlarms; i++)
+  {
+    sprintf(setting_alarms[i-1], setting_alarms[i]); 
+    music_options[i-1] = music_options[i]; 
+  }
+  currNumberAlarms-=1;
+  *setting_alarms[currNumberAlarms] = '\0';
+  music_options[currNumberAlarms] = -1;
+  // *setting_alarms[alarm_id] = '\0';
+  // music_options[alarm_id] = -1;
 }
 
 void print_time(int hour, int minute){
   if (hour > 9 and minute > 9){
-    tft.printf("Time:%d:%d", hour, minute);
+    tft.printf("%d:%d", hour, minute);
   }
   else if (minute > 9){
-    tft.printf("Time:0%d:%d", hour, minute);
+    tft.printf("0%d:%d", hour, minute);
   }
   else if (hour > 9){
-    tft.printf("Time:%d:0%d", hour, minute);
+    tft.printf("%d:0%d", hour, minute);
   }
   else{
-    tft.printf("Time:0%d:0%d", hour, minute);
+    tft.printf("0%d:0%d", hour, minute);
   }
-   
+}
+
+void time_to_str(char* time_arr, int hour, int minute){
+  if (hour > 9 and minute > 9){
+    sprintf(time_arr, "%d:%d", hour, minute);
+  }
+  else if (minute > 9){
+    sprintf(time_arr, "0%d:%d", hour, minute);
+  }
+  else if (hour > 9){
+    sprintf(time_arr, "%d:0%d", hour, minute);
+  }
+  else{
+    sprintf(time_arr, "0%d:0%d", hour, minute);
+  }
 }
 
 void modify_prints(int hour, int min, int music_option){
@@ -174,59 +232,38 @@ void modify_prints(int hour, int min, int music_option){
   tft.println("");
 }
 
-void modify_alarm(int alarm_num)
-{  
-  // char time_alarm[50];
-  
-  button45.read();
-  if (button45.button_pressed && millis() - button45.button_change_time > BUTTON_PRESS_BUFFER)
+void modify_alarm_2(int alarm_num)
+{
+  if (alarm_state == ADD_HOUR)
   {
-    button45.button_change_time = millis();
-    Serial.printf("Hour %d", hour);
-    Serial.println("");
-    hour+=1;
-    if (hour == 24){
-      hour = 0;
+    float x, y;
+    get_angle(&x, &y); //get angle values
+    int bv = button39.update(); //get button value
+    int res = tg.update(y, bv, 23); //input: angle and button, output String to display on 
+    if (res != -1)  {
+      hour = res;
+      alarm_state = ADD_MIN;
     }
-    modify_prints(hour, mins, music_option);
   }
-
-  button39.read();
-  if (button39.button_pressed && millis() - button39.button_change_time > BUTTON_PRESS_BUFFER)
+  if (alarm_state == ADD_MIN)
   {
-    button39.button_change_time = millis();
-    Serial.printf("Mins %d", mins);
-    Serial.println("");
-     mins+=1;
-    if (mins == 60){
-      mins = 0;
+    float x, y;
+    get_angle(&x, &y); //get angle values
+    int bv = button39.update(); //get button value
+    int res = tg.update(y, bv, 59); //input: angle and button, output String to display on 
+    if (res != -1)  {
+      mins = res;
+      alarm_state = ADD_MUSIC;
+      time_to_str(setting_alarms[alarm_num], hour, mins);
     }
-    modify_prints(hour, mins, music_option);
   }
+  if (alarm_state == ADD_MUSIC){
+    music_options[alarm_num] = 1;
 
-  button38.read();
-  if (button38.button_pressed && millis() - button38.button_change_time > BUTTON_PRESS_BUFFER)
-  {
-    button38.button_change_time = millis();
-    button38.update();
-    Serial.println("");
-    music_option+=1;
-    modify_prints(hour, mins, music_option);
-  }
-
-  button34.read();
-  if (button34.button_pressed && millis() - button34.button_change_time > BUTTON_PRESS_BUFFER)
-  {
-    button34.button_change_time = millis();
-    alarm_hours[alarm_num] = hour;
-    alarm_minutes[alarm_num] = mins;
-    music_options[alarm_num] = music_option;
-    Serial.printf("Minutes Set %d", mins);
-    Serial.printf("Min Set %d", alarm_minutes[alarm_num]);
-    Serial.printf("Music Set %d", music_options[alarm_num]);
     if (setting_state == ADD){
       currNumberAlarms++;
     }
     setting_state = PRINT_ALARMS;
+    alarm_state = ADD_HOUR;
   }
 }
